f389bbd827442b113578fa29ba37364f19e30be6
againme <againme@againmeui-Mac-mini.local>
2018-09-07 10:19:03 +0900

    02 second

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -8,15 +8,17 @@ import { Button, Card, CardSection, Input } from './common/';
 
 class LoginForm extends Component {
 
-    state = { text: ''};
+    state = { email: ''};
 
     render() {
         return (
             <Card>
                 <CardSection>
                     <Input 
-                    value={this.state.text}
-                    onChangeText={ text => this.setState({text}) }
+                    placeholder="email here!!"
+                    label="Email"
+                    value={this.state.email}
+                    onChangeText={ email => this.setState({ email }) }
                     />
                 </CardSection>
 
